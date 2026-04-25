eda00be927cc7bc609d2364de0056e0899d45d9e
christianskrede-maker <christianskrede@gmail.com>
2026-04-25 10:16:07 +0200

    Update route.ts

--- app/api/innspill/route.ts
+++ app/api/innspill/route.ts
@@ -1,14 +1,14 @@
 import { redirect } from "next/navigation";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(request: Request) {
   const formData = await request.formData();
 
   const kapittel = String(formData.get("kapittel") || "Ukjent kapittel");
   const innspill = String(formData.get("innspill") || "");
   const kontaktinfo = String(formData.get("kontaktinfo") || "Anonym");
+
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
   await resend.emails.send({
     from: "Asker Venstre <onboarding@resend.dev>",
