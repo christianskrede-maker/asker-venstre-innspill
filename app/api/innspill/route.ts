1ec9703b28074c1d5d7a363ced43eaf30be81211
christianskrede-maker <christianskrede@gmail.com>
2026-04-25 10:12:13 +0200

    Update route.ts

--- app/api/innspill/route.ts
+++ app/api/innspill/route.ts
@@ -1,1 +1,32 @@
+import { redirect } from "next/navigation";
+import { Resend } from "resend";
 
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+
+  const kapittel = String(formData.get("kapittel") || "Ukjent kapittel");
+  const innspill = String(formData.get("innspill") || "");
+  const kontaktinfo = String(formData.get("kontaktinfo") || "Anonym");
+
+  await resend.emails.send({
+    from: "Asker Venstre <onboarding@resend.dev>",
+    to: "christianskrede@gmail.com",
+    subject: `Innspill til ${kapittel}`,
+    text: `
+Nytt innspill til Asker Venstres programarbeid
+
+Kapittel:
+${kapittel}
+
+Innspill:
+${innspill}
+
+Kontaktinfo:
+${kontaktinfo}
+    `,
+  });
+
+  redirect("/");
+}
